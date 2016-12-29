1a513cb11b9667ccba1ba1dea04618350db8dd3a
econtreras <econtreras@INFORMATICADELL>
2016-12-29 10:54:45 -0300

    Add project files.

---
+++ FacturacionMasiva.Hofmann/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace FacturacionMasiva.Hofmann
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvPicking = new System.Windows.Forms.DataGridView();
+            this.btnProcesar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dgvFacturas = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnOpenExcel = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnCargarExcel = new System.Windows.Forms.Button();
+            this.Picking = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Factura = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPicking)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFacturas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvPicking
+            // 
+            this.dgvPicking.AllowUserToAddRows = false;
+            this.dgvPicking.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPicking.Location = new System.Drawing.Point(6, 17);
+            this.dgvPicking.Name = "dgvPicking";
+            this.dgvPicking.ReadOnly = true;
+            this.dgvPicking.Size = new System.Drawing.Size(188, 419);
+            this.dgvPicking.TabIndex = 0;
+            // 
+            // btnProcesar
+            // 
+            this.btnProcesar.Location = new System.Drawing.Point(268, 232);
+            this.btnProcesar.Name = "btnProcesar";
+            this.btnProcesar.Size = new System.Drawing.Size(95, 61);
+            this.btnProcesar.TabIndex = 1;
+            this.btnProcesar.Text = "Procesar";
+            this.btnProcesar.UseVisualStyleBackColor = true;
+            this.btnProcesar.Click += new System.EventHandler(this.btnProcesar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dgvPicking);
+            this.groupBox1.Location = new System.Drawing.Point(21, 82);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 442);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Picking";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dgvFacturas);
+            this.groupBox2.Location = new System.Drawing.Point(386, 82);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(311, 436);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Facturas Generadas";
+            // 
+            // dgvFacturas
+            // 
+            this.dgvFacturas.AllowUserToAddRows = false;
+            this.dgvFacturas.AllowUserToDeleteRows = false;
+            this.dgvFacturas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFacturas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Picking,
+            this.Factura});
+            this.dgvFacturas.Location = new System.Drawing.Point(6, 19);
+            this.dgvFacturas.Name = "dgvFacturas";
+            this.dgvFacturas.ReadOnly = true;
+            this.dgvFacturas.Size = new System.Drawing.Size(299, 411);
+            this.dgvFacturas.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(342, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 13);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "** Solo Excel 97-2003";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Ruta:";
+            // 
+            // btnOpenExcel
+            // 
+            this.btnOpenExcel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnOpenExcel.Location = new System.Drawing.Point(289, 24);
+            this.btnOpenExcel.Name = "btnOpenExcel";
+            this.btnOpenExcel.Size = new System.Drawing.Size(34, 24);
+            this.btnOpenExcel.TabIndex = 22;
+            this.btnOpenExcel.Text = "...";
+            this.btnOpenExcel.UseVisualStyleBackColor = true;
+            this.btnOpenExcel.Click += new System.EventHandler(this.btnOpenExcel_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(93, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(190, 20);
+            this.textBox1.TabIndex = 21;
+            // 
+            // btnCargarExcel
+            // 
+            this.btnCargarExcel.Location = new System.Drawing.Point(458, 18);
+            this.btnCargarExcel.Name = "btnCargarExcel";
+            this.btnCargarExcel.Size = new System.Drawing.Size(101, 37);
+            this.btnCargarExcel.TabIndex = 25;
+            this.btnCargarExcel.Text = "Cargar Excel";
+            this.btnCargarExcel.UseVisualStyleBackColor = true;
+            this.btnCargarExcel.Click += new System.EventHandler(this.btnCargarExcel_Click);
+            // 
+            // Picking
+            // 
+            this.Picking.HeaderText = "Picking";
+            this.Picking.Name = "Picking";
+            this.Picking.ReadOnly = true;
+            // 
+            // Factura
+            // 
+            this.Factura.HeaderText = "Factura";
+            this.Factura.Name = "Factura";
+            this.Factura.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(721, 589);
+            this.Controls.Add(this.btnCargarExcel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnOpenExcel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnProcesar);
+            this.Name = "Form1";
+            this.Text = "Facturación Masiva";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPicking)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFacturas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvPicking;
+        private System.Windows.Forms.Button btnProcesar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dgvFacturas;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnOpenExcel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnCargarExcel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Picking;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Factura;
+    }
+}
+
---
+++ FacturacionMasiva.Hofmann/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FacturacionMasiva.Hofmann
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void generarFactura(DataTable dtFactura,string NumFactura)
+        {
+
+            using (StringWriter sw = new StringWriter())
+            {
+                foreach (DataRow row in dtFactura.Rows)
+                {
+                    sw.WriteLine(row[1]);
+                    using (FileStream fs = new FileStream(@"\\zeus\Emisión Offline IC - Producción\Entrada\33_NPG_" + NumFactura+"+_.txt", FileMode.OpenOrCreate, FileAccess.Write))
+                    {
+                        byte[] buffer = System.Text.Encoding.Default.GetBytes(sw.ToString());
+                        fs.Write(buffer, 0, buffer.Length);
+
+                    }
+                }
+            }
+
+
+
+        }
+
+        private void btnProcesar_Click(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            for (int i = 0; i < dgvPicking.RowCount; i++)
+            {
+
+                using (SqlConnection cn = new SqlConnection("Data Source=200.6.76.250;Initial Catalog=SLAPPHOFMANN;;Persist Security Info=True;User ID=us"))
+                {
+                    cn.Open();
+
+                    SqlCommand cm = new SqlCommand("exec xsp_LE_Obtener_Correlativo 'FEL','DHOFMANN'", cn);
+                    int correlativo = Convert.ToInt32(cm.ExecuteScalar());
+
+                    string NumFactura = "F" + correlativo.ToString("0000000");
+
+
+                    SqlDataAdapter da = new SqlDataAdapter("exec xsp_GenerarFactura '" + dgvPicking.Rows[i].Cells[0].Value.ToString() + "','" + NumFactura + "'", cn);
+                    DataTable dtFactura = new DataTable();
+                    da.Fill(dtFactura);
+
+                    generarFactura(dtFactura, NumFactura);
+
+                    dgvFacturas.Rows.Add(dgvPicking.Rows[i].Cells[0].Value.ToString(), NumFactura);
+
+
+
+
+                }
+            }
+
+
+            
+            dgvPicking.DataSource = "";
+            dgvPicking.DataSource = null;
+            Cursor.Current = Cursors.Default;
+            MessageBox.Show("Proceso Concluido");
+        }
+
+        private void btnOpenExcel_Click(object sender, EventArgs e)
+        {
+
+            dialog.Filter = "Excel files (*.xls)|*.xls|All files (*.*)|*.*";
+            dialog.InitialDirectory = "C:";
+            dialog.Title = "Seleccione un Excel Version 2003";
+
+            string strFileName;
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                strFileName = dialog.FileName;
+                textBox1.Text = strFileName;
+            }
+            else
+            {
+                textBox1.Text = "";
+                return;
+
+            }
+            //if (strFileName == String.Empty)
+            //{
+            //    return; //user didn't select a file to opena
+            //}
+            sConnectionString = "Provider=Microsoft.Jet.OleDb.4.0; Data Source = " + strFileName + "; Extended Properties = \"Excel 8.0;HDR=Yes;IMEX=1\";";
+
+            //sConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + strFileName + ";Extended Properties=Excel 12.0 Xml;HDR=YES;";
+        }
+
+
+        OpenFileDialog dialog = new OpenFileDialog();
+        String input = string.Empty;
+        string sConnectionString;
+        private void btnCargarExcel_Click(object sender, EventArgs e)
+        {
+
+            OleDbConnection cnx = new OleDbConnection(sConnectionString);
+            OleDbDataAdapter da = new OleDbDataAdapter("select * from [Hoja1$]", cnx);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            dgvPicking.DataSource = dt;
+        }
+    }
+}
